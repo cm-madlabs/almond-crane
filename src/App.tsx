a49fb0315bbd59f87fa6ee06bbf743113d6c929a
intercept6 <intercept6@gmail.com>
2020-05-01 15:18:51 +0900

    コース一覧をルーティングに接続する

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,13 @@
-import React, { FC } from 'react';
-import { Course } from './interfaces';
-
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   useParams,
 } from 'react-router-dom';
-import { ListCoursesBody } from './components/Organisms/body/list-cources';
+import { ListCoursesPageContainer } from './components/pages/list-cources';
 
-const sampleRouteData: Course[] = [
-  {
-    id: '1',
-    name: 'ルート１',
-    departure: '出発地',
-    arrival: '目的地',
-    notification: true,
-    schedule: {},
-    timeTable: {},
-    requiredMinutes: 10,
-  },
-  {
-    id: '2',
-    name: '会社 → 自宅',
-    departure: '会社 (岩本町オフィス)',
-    arrival: '岩本町',
-    notification: true,
-    schedule: {},
-    timeTable: {},
-    requiredMinutes: 10,
-  },
-  {
-    id: '3',
-    name: '自宅 → 会社',
-    departure: '自宅',
-    arrival: 'XXX駅',
-    notification: true,
-    schedule: {},
-    timeTable: {},
-    requiredMinutes: 5,
-  },
-];
-
-const data: Course[] = [...sampleRouteData];
-
-const CourseDetail: FC = () => {
+const CourseDetail: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   return <h1>This course id is {id}</h1>;
 };
@@ -57,7 +20,7 @@ function App() {
           <h1>Home</h1>
         </Route>
         <Route exact path="/courses">
-          <ListCoursesBody courses={data} />
+          <ListCoursesPageContainer />
         </Route>
         <Route exact path="/courses/add">
           <p>new route</p>
---
+++ src/components/pages/list-cources/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { BaseTemplate } from '../../templates/base-template';
+import { HeaderProps } from '../../Organisms/header';
+import {
+  ListCoursesBody,
+  ListCoursesProps,
+} from '../../Organisms/body/list-cources';
+import { Course } from '../../../interfaces';
+
+type ListCoursesPagePresentationalProps = {
+  body: ListCoursesProps;
+  header: HeaderProps;
+};
+
+export const ListCoursesPagePresentational: React.FC<ListCoursesPagePresentationalProps> = ({
+  header,
+  body,
+}) => {
+  const main: React.ReactElement = (
+    <>
+      <ListCoursesBody {...body} />
+    </>
+  );
+
+  return <BaseTemplate header={header} main={main} />;
+};
+
+export const ListCoursesPageContainer: React.FC = () => {
+  const header: HeaderProps = {
+    title: 'コース一覧',
+    onMenuButtonClick: () => console.log('メニューがクリックされました'),
+  };
+
+  const courses: Course[] = [
+    {
+      id: '1',
+      name: 'ルート１',
+      departure: '出発地',
+      arrival: '目的地',
+      notification: true,
+      schedule: {},
+      timeTable: {},
+      requiredMinutes: 10,
+    },
+    {
+      id: '2',
+      name: '会社 → 自宅',
+      departure: '会社 (岩本町オフィス)',
+      arrival: '岩本町',
+      notification: true,
+      schedule: {},
+      timeTable: {},
+      requiredMinutes: 10,
+    },
+    {
+      id: '3',
+      name: '自宅 → 会社',
+      departure: '自宅',
+      arrival: 'XXX駅',
+      notification: true,
+      schedule: {},
+      timeTable: {},
+      requiredMinutes: 5,
+    },
+  ];
+
+  const body: ListCoursesProps = {
+    courses,
+  };
+
+  return <ListCoursesPagePresentational header={header} body={body} />;
+};
